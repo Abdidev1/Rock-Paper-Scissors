c6e4c5d8ad79516342f87e717064444fba31fa85
Abdullah Mohsin <abdullahmohsin077@gmail.com>
2026-06-20 14:53:56 +0500

    dom elements

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ document.addEventListener("DOMContentLoaded", function(){
         "paper": "🖐🏼",
         "scissors": "✌🏼"
     };
+
+    const currentModeDisplayElement = document.getElementById('currentModeDisplay');
+    const statusAnnouncementElement = document.getElementById('roundOutcomeMessage');
+    const targetScoreInputElement = document.getElementById('targetScoreInput');
+
+    const enterScoreLimitButton = document.getElementById('confirmScoreButton');
+    const masterResetButton = document.getElementById('masterResetButton');
+    const allWeaponButtons = document.querySelectorAll(".player-weapon-button");
+
+    const numericScoreDisplayPlayer = document.getElementById('playerScoreCounter');
+    const numericScoreDisplayRobot = document.getElementById('robotScoreCounter');
+
+    const animationBattleStageElement = document.getElementById('animationBattleStage');
+    const playerAnimatedHandElement = document.getElementById('playerAnimatedHand');
+    const robotAnimatedHandElement = document.getElementById('robotAnimatedHand');
 })
