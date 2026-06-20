caf4ea3b152ec9881a832cacaeda7978f6cff860
Abdullah Mohsin <abdullahmohsin077@gmail.com>
2026-06-20 14:11:16 +0500

    global application

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    console.log("Rock Paper Scissors game is ready.");
+
+    let currentScorePlayer = 0;
+    let currentScoreRobot = 0;
+    let maximumTargetPoints = null;
+    let isTheGameLockedOut = false;
+})
